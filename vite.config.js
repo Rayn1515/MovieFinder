16d969d4d7128ae7192b0cf83bdef2f510a9f549
Rayn1515 <devanshinr15@gmail.com>
2025-03-25 18:23:37 +0530

    e

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import jsconfigPaths from "vite-jsconfig-paths"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),jsconfigPaths()],
-  base: '/movieFinder/',
   build: {
+    base: '/movieFinder/',
     chunkSizeWarningLimit: 1000, // Increase the chunk size limit to 1000 KB (default is 500 KB)
   },
 })
